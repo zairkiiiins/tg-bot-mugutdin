e4333feddc3c2832b27a79d69634a72022eb5c71
zairkiiiins <zairkam12@gmail.com>
2025-01-02 17:30:23 +0300

    ъхдююхъд юхзджюбдждоьдлогоглаг

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module example.com/m/v2
 
 go 1.23.3
 
-require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+require rsc.io/quote v1.5.2
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
--- hello.go
+++ hello.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
-
+import (
+	"log"
+	"os"
+	"github.com/go-telegram-bot-api/telegram-api/v5"
+)
 func main() {
- fmt.Println("Hello, world.")
+	bot, err := tgbotapi.NewBotAPI("7927809753:AAGaYqVDw7MY0eYDaaU-0899Zg7k-uPSj6Q")
+	if err !=nil{
+		log.Panic(err)
+	} 
+		
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.@mugutdin),
+
+	u := tgbotapi. NewUpdate(0)
+	u.Timeout=60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nill {
+			log.Printf("[%s] %s", update.Message.From.@mugutdin,update.Message.Text)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID,update.Message.Text)
+			bot.Send(msg)
+		}
+	}
 }
+for update := range updates {
+	if update.Message != nil {
+		log.Printf("[%s] %s", update.Message.From.@mugutdin, update.Message.Text)
+
+		var msg tgbotapi.MessageConfig
+
+		switch update .Message.Command() {
+		case "start" :
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID,"Welcome! I am your bot.")
+		case "help":
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID,"I can help you with the following commands:\n/start - Start the bot\n/help - Display this help message")
+			default:
+			    msg = tgbotapi.NewMessage(update.Message.Chat.ID, "I don't know that command")
+			}
+
+			bot.Send(msg)
+		}
+	}
